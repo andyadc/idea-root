b2111c30bb8b12a19539a13513f12ea9e5ba46b1
andaicheng <andaicheng@gmail.com>
2016-11-23 13:42:25 +0800

    initial showcase

---
+++ idea-examples/showcase/src/main/java/com/idea/showcase/util/ThreadUtils.java
@@ -0,0 +1,22 @@
+package com.idea.showcase.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThreadUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(ThreadUtils.class);
+
+    public static void sleepOneSecond() {
+        sleepSeconds(1);
+    }
+
+    public static void sleepSeconds(int seconds) {
+        try {
+            logger.debug("Thread {} sleep {} seconds...", Thread.currentThread().getName(), seconds);
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
